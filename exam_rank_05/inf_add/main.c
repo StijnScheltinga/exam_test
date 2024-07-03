079b045c7595587e7393ba64ec59ebf77eb70ca1
StijnScheltinga <stijnskoop@gmail.com>
2024-07-03 18:24:22 +0200

    start on inf multiply

--- exam_rank_05/inf_add/main.c
+++ exam_rank_05/inf_add/main.c
@@ -1,107 +1,123 @@
-#include "inf_add.h"
+#include "stdlib.h"
+#include "unistd.h"
+#include "stdio.h"
 
-char *truncate_zero(char *result)
+int	ft_strlen(char *str)
 {
-	char *trim = malloc((ft_strlen(result) - 1) * sizeof(char));
-	int trimindex = 0;
 	int i = 0;
-	for (int i = 1; result[i]; i++)
-		trim[trimindex++] = result[i];
-	free(result);
-	return trim;
+	while (str[i])
+		i++;
+	return(i);
 }
 
-/*
-how to subtract numbers:
-num1 = 148292 num2 = -6630982
-
-*/
-
-
-char *add(char *s1, char *s2)
+int	s2_is_bigger(char *num1, char *num2)
 {
-	int maxLen = max_length(s1, s2);
+	if (ft_strlen(num2) > ft_strlen(num1))
+		return 1;
+	else if (ft_strlen(num2) == ft_strlen(num1))
+	{
+		for (int i = 0; num1[i]; i++)
+		{
+			if (num2[i] - '0' > num1[i] - '0')
+				return 1;
+			else if (num2[i] - '0' < num1[i] - '0')
+				return 0;
+		}
+	}
+	return 0;
+}
 
-	//allocate result string two more bytes then longest string, '\0' and carry over
-	char *result = malloc(sizeof(char) * (maxLen + 2));
-	int i = maxLen + 1;
-	result[i] = '\0';
-	i--;
+char	*truncate_zeros(char *untrimmed)
+{
+	if (untrimmed[0] != '0')
+		return untrimmed;
+	
+	int zero_n = 0;
+	while (untrimmed[zero_n] && untrimmed[zero_n] == '0')
+		zero_n++;
+	
+	int 	trimmedlen = ft_strlen(untrimmed) - zero_n;
+	char	*trimmed = malloc(sizeof(char) * trimmedlen + 1);
+	trimmed[trimmedlen] = '\0';
 
-	//create index for both strings
-	int s1index = ft_strlen(s1) - 1;
-	int s2index = ft_strlen(s2) - 1;
+	for (int i = 0; i < trimmedlen; i++)
+		trimmed[i] = untrimmed[i + zero_n];
+
+	free(untrimmed);
+	return (trimmed);
+}
+
+char	*add(char *num1, char *num2)
+{
+	int	num1len = ft_strlen(num1);
+	int	num2len = ft_strlen(num2);
+	int maxlen = num1len > num2len ? num1len : num2len;
+
+	char *result = malloc(sizeof(char) * (maxlen + 2));
+	int	index = maxlen + 1;
+	result[index] = '\0';
+	index--;
+	
+	int num1index = ft_strlen(num1) - 1;
+	int num2index = ft_strlen(num2) - 1;
+
+	int digit1 = 0;
+	int digit2 = 0;
 
 	int carry = 0;
 	int sum = 0;
-	//start at back of both strings and calculate their sum
-	while (i >= 0)
+
+	while (index >= 0)
 	{
-		//calulate sum of the two characters
-		int digit1 = s1index >= 0 ? s1[s1index] - '0' : 0;
-		int digit2 = s2index >= 0 ? s2[s2index] - '0' : 0;
-		sum = digit1 + digit2 + carry; // sum = digit1 - digit2 - carry
+		digit1 = num1index >= 0 ? num1[num1index] - '0' : 0;
+		digit2 = num2index >= 0 ? num2[num2index] - '0' : 0;
+		sum = digit1 + digit2 + carry;
 		carry = sum / 10;
 		sum = sum % 10;
-		result[i] = sum + '0';
-		i--;
-		s1index--;
-		s2index--;
+		result[index] = sum + '0';
+		index--;
+		num1index--;
+		num2index--;
 	}
-	if (carry != 0)
-		result[0] = carry + '0';
-	else
-		return truncate_zero(result);
+	result = truncate_zeros(result);
 	return result;
 }
 
-//negative number always comes second
-char *subtract(char *s1, char *s2)
+char	*subtract(char *num1, char *num2)
 {
-	int s1len = ft_strlen(s1);
-	int s2len = ft_strlen(s2);
-	int maxLen = max_length(s1, s2);
-
-	//allocate result string two more bytes then longest string, '\0' and carry over
-	char *result = malloc(sizeof(char) * (maxLen + 2));
-	if (!result)
-		return NULL;
-	int i = maxLen + 1;
-	result[i] = '\0';
-	i--;
-
-
-	//if number to be subtracted is bigger we should rearrenge the numbers and print a '-' sign at the end
 	int sign = 1;
-
-	// printf("before swap, s1: %s, s2: %s\n", s1, s2);
-
-	if (s2IsBigger(s1, s2))
+	if (s2_is_bigger(num1, num2))
 	{
-		// write(1, "string 2 is bigger\n", 19);
-		char *temp = s1;
-		s1 = s2;
-		s2 = temp;
-		int tempi = s1len;
-		s1len = s2len;
-		s2len = tempi;
+		char *temp = num2;
+		num2 = num1;
+		num1 = temp;
 		sign = -1;
 	}
 
-	// printf("after swap, s1: %s, s2: %s\n", s1, s2);
-	//create index for both strings
-	int s1index = s1len - 1;
-	int s2index = s2len - 1;
+	int	num1len = ft_strlen(num1);
+	int	num2len = ft_strlen(num2);
+	int maxlen = num1len > num2len ? num1len : num2len;
+
+	char	*result = malloc(sizeof(char) * maxlen + 2);
+	int	index = maxlen + 1;
+	result[index] = '\0';
+	index--;
+
+	int num1index = ft_strlen(num1) - 1;
+	int num2index = ft_strlen(num2) - 1;
+
+	int digit1 = 0;
+	int digit2 = 0;
 
 	int carry = 0;
 	int sum = 0;
-	//start at back of both strings and calculate their sum
-	while (i >= 0)
+
+
+	while (index >= 0)
 	{
-		//calulate sum of the two characters
-		int digit1 = s1index >= 0 ? s1[s1index] - '0' : 0;
-		int digit2 = s2index >= 0 ? s2[s2index] - '0' : 0;
-		sum = digit1 - digit2 - carry; // sum = digit1 - digit2 - carry
+		digit1 = num1index >= 0 ? num1[num1index] - '0' : 0;
+		digit2 = num2index >= 0 ? num2[num2index] - '0' : 0;
+		sum = digit1 - digit2 - carry;
 		if (sum < 0)
 		{
 			carry = 1;
@@ -109,57 +125,40 @@ char *subtract(char *s1, char *s2)
 		}
 		else
 			carry = 0;
-		result[i] = sum + '0';
-		i--;
-		s1index--;
-		s2index--;
+		result[index] = sum + '0';
+		index--;
+		num1index--;
+		num2index--;
 	}
-	// return truncate_zero(result);
 	if (sign == -1)
-		write(1, "-", 1);
-	result = trimZeros(result);
+		write (1, "-", 1);
+	result = truncate_zeros(result);
 	return result;
 }
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
 	if (argc != 3)
-	{
-		printf("program requires 2 numbers\n");
 		return 1;
-	}
-	char *string1 = argv[1];
-	char *string2 = argv[2];
 
-	char sign1 = string1[0];
-	char sign2 = string2[0];
-
-	char *result;
+	char	*number1 = argv[1];
+	char	*number2 = argv[2];
+	char	sign1 = number1[0];
+	char	sign2 = number2[0];
+	char	*result;
 
 	if (sign1 == '-' && sign2 == '-')
 	{
-		result = add(&string1[1], &string2[1]);
-		printf("result: -%s\n", result);	
+		result = add(&number1[1], &number2[1]);
+		write(1, "-", 1);
 	}
 	else if (sign1 == '-' && sign2 != '-')
-	{
-		// printf("first number is negative\n");
-		result = subtract(string2, &string1[1]);
-		printf("%s\n", result);
-	}
+		result = subtract(number2, &number1[1]);
 	else if (sign1 != '-' && sign2 == '-')
-	{
-		// printf("second number is negative\n");
-		result = subtract(string1, &string2[1]);
-		printf("%s\n", result);
-	}
+		result = subtract(number1, &number2[1]);
 	else
-	{
-		result = add(string1, string2);
-		printf("result: %s\n", result);
-	}
-
+		result = add(number1, number2);
+	write(1, result, ft_strlen(result));
+	write(1, "\n", 1);
 	free(result);
-	// result = subtract("9999234", "-99999");
-	// printf("result: %s\n", result);
 }
---
+++ exam_rank_05/inf_multiply/main.c
@@ -0,0 +1,55 @@
+#include "stdlib.h"
+#include "stdio.h"
+
+int	ft_strlen(char *str)
+{
+	int i = 0;
+	while (str[i])
+		i++;
+	return i;
+}
+
+char *reverse_string(char *string)
+{
+	int len = ft_strlen(string);
+	for (int i = 0; i < len / 2; i++)
+	{
+		char temp = string[i];
+		string[i] = string[len - 1 - i];
+		string[len - 1 - i] = temp;
+	}
+	return string;
+}
+
+char	*multiply(char *num1, char *num2)
+{
+	int	num1len = ft_strlen(num1);
+	int	num2len = ft_strlen(num2);
+	int	resultlen = num1len + num2len;
+	int	*result = malloc(sizeof(int) * (resultlen + 1));
+
+	//initialize result array to 0
+	for (int i = 0; i != resultlen; i++)
+		result[i] = 0;
+
+	num1 = reverse_string(num1);
+	num2 = reverse_string(num2);
+
+	
+	for (int i = 0; i < num1len; i++)
+		for (int j = 0; j < num2len; j++)
+		{
+
+		}
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc != 3)
+		return 1;
+	char	*num1 = argv[1];
+	char	*num2 = argv[2];
+	char	sign1 = num1[0];
+	char	sign2 = num2[0];
+	multiply(num1, num2);
+}
